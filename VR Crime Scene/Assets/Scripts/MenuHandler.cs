ce6cff2633cf1b1703f947d9b570b27ce2cfd4f8
hsyuting <hsyuting@gmail.com>
2018-11-27 13:46:06 -0800

    after demo changes
    new menu prototype

---
+++ VR Crime Scene/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class MenuHandler : MonoBehaviour {
+
+    private Hand hand;
+    private bool flag = true;
+    public GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+        hand = gameObject.GetComponent<Hand>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Input._default.inActions.Teleport.GetStateDown(hand.handType))
+        {
+            menu.SetActive(flag);
+            flag = !flag;
+        }
+    }
+}
