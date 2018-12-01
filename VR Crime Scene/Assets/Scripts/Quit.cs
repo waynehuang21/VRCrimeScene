ab5ede62c26865f83a86db8bd19d023d50ae5023
Wayne Huang <wayne.huang.21@gmail.com>
2018-11-30 19:01:21 -0800

    Moved reset into right hand menu and added quit button

---
+++ VR Crime Scene/Assets/Scripts/Quit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Quit : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnHandHoverBegin(Hand hand)
+    {
+        Application.Quit();
+    }
+}
